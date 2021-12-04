d82f7cd62846c6ca32a8a0f38dba290c54776a5b
Seamus Riordan <seamus@riordan.io>
2021-12-04 13:28:54 -0600

    Refactor and solution to part 2

--- src/day_02/core.clj
+++ src/day_02/core.clj
@@ -2,13 +2,23 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
+(defn update-forward-with-aim
+  [position value]
+  (let [aim (:aim position)
+        aim-value-product (* aim value)
+        updated-horiz (update position :horiz + value)
+        updated-depth (update updated-horiz :depth + aim-value-product)]
+    updated-depth
+    ))
+
+
 (defn apply-command
   [position command]
   (let [value (:value command)]
     (condp = (:command command)
-      "forward" (update (update position :horiz #(+ % value)) :depth #(+ % (* (:aim position) value)))
-      "up" (update position :aim #(- % value))
-      "down" (update position :aim #(+ % value))
+      "forward" (update-forward-with-aim position value)
+      "up" (update position :aim - value)
+      "down" (update position :aim + value)
       ))
   )
 
@@ -17,13 +27,12 @@
   [input]
   (let [lines (str/split-lines input)
         tokens (map #(str/split % #"\s+") lines)
-        command-pairs (map #(vector (first %) (Integer. (second %))) tokens)
+        command-pairs (map #(vector (first %) (Integer/parseInt (second %))) tokens)
         commands (map #(zipmap [:command :value] %) command-pairs)]
     (reduce apply-command {:horiz 0 :depth 0 :aim 0} commands)
-    ;commands
     ))
 
-(defn main-
+(defn -main
   []
   (let [lines (slurp (io/resource "input.txt"))]
     (get-position lines)))
